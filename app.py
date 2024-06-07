8d016f06e035abcbf41899df56014439f5bee365
Frida <jacobson_frida@hotmail.com>
2024-06-07 13:08:12 +0200

    0000: 5000

--- app.py
+++ app.py
@@ -1,65 +1,72 @@
-from flask import Flask, render_template, send_file, jsonify
+#!/usr/bin/env python
+# coding: utf-8
+
+from flask import Flask, render_template
+from flask import send_file
 from kpi_calculator import get_kpis
+import logging
 import sys
 import zipfile
-import os
-import io
-import time
-import threading
 
-# T capture print statements, with buffering
-class BufferedIOHandler(io.TextIOBase):
+# For print statement redirects
+class RedirectStdoutToFile:
     def __init__(self, file_path):
-        self.buffer = io.StringIO()
         self.file_path = file_path
+        self.original_stdout = sys.stdout
 
-    def write(self, s):
-        self.buffer.write(s)
-        if '\n' in s:
-            self.flush()
+    def __enter__(self):
+        self.file = open(self.file_path, 'w')
+        sys.stdout = self.file
 
-    def flush(self):
-        with open(self.file_path, 'a') as f:
-            f.write(self.buffer.getvalue())
-        self.buffer.seek(0)
-        self.buffer.truncate()
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        sys.stdout = self.original_stdout
+        self.file.close()
 
 app = Flask(__name__)
 
+# Configure logging
+#logging.basicConfig(filename='kpis.log', level=logging.INFO, format='%(message)s')
+
+# Route for the index page
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html') 
 
-# Function to run KPI calculations and create zip file
 def run_kpi_calc():
-    with BufferedIOHandler('pps_data.log') as log_buffer:
-        sys.stdout = log_buffer
+    # Capture the print statements for log file
+    with RedirectStdoutToFile('pps_data.log'):
         try:
+            # Run the KPI script function
             get_kpis()
         except Exception as e:
             print("An error occurred while running kpi_calculator.py:", e)
 
+# In your Flask route, call the function to run kpi_calculator.py
+#@app.route('/run_kpi_calc')
+#def execute_kpi_calc():
+#    run_kpi_calc()
+#    return "kpi_calculator.py executed successfully"
+
+# Route to handle file download
+@app.route('/download')
+def download_zip_file():
+    # Run the function to generate KPIs and log output
+    run_kpi_calc()
+    
+    # Create a zip file and add the Excel file and the log file to it
     with zipfile.ZipFile('KPI_files.zip', 'w') as zipf:
         zipf.write('kpis_VR.xlsx')
         zipf.write('pps_data.log')
-
-# Route to trigger and handle file download
-@app.route('/download')
-def trigger_and_download():
-
-    if not os.path.exists('KPI_files.zip'):
-        threading.Thread(target=run_kpi_calc).start()
-
-    # Check continuously if zip exists, timeout after 1 minute
-    timeout = 60  # 1 minute
-    start_time = time.time()
-
-    while not os.path.exists('KPI_files.zip'):
-        if time.time() - start_time > timeout:
-            return "Oops, something went wrong", 404
-        time.sleep(1)  # Check every second
-    
+        
     return send_file('KPI_files.zip', as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
+
+
+
+
+
+
+
+
