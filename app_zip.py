bff841926c37bf2fd16d4562b4e7c82d5193a528
Frida <jacobson_frida@hotmail.com>
2024-06-25 14:02:55 +0200

    App is now removing produced files after request

--- app_zip.py
+++ app_zip.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, send_file
+from flask import Flask, render_template, send_file, abort, after_this_request
 from kpi_calculator import get_kpis
 import sys
 import zipfile
@@ -37,7 +37,10 @@ def run_kpi_calc():
         try:
             get_kpis()
         except Exception as e:
-            print("An error occurred while running kpi_calculator.py:", e)
+            print(f'An error occurred while running kpi_calculator.py: {e}')
+        finally:
+            log_buffer.flush()
+            sys.stdout = sys.__stdout__  # Reset stdout to default
 
     with zipfile.ZipFile('KPI_files.zip', 'w') as zipf:
         zipf.write('kpis_VR.xlsx')
@@ -59,14 +62,25 @@ def trigger_and_download():
         if time.time() - start_time > timeout:
             return "File not generated", 404
         time.sleep(1)  # Check every second
-    
+
+    @after_this_request
+    # After try / except block with send_file
+    # remove_files will automatically be called
+    def remove_files(response):
+        try:
+            os.remove('KPI_files.zip')
+            os.remove('kpis_VR.xlsx')
+            os.remove('pps_data.log')
+        except Exception as e:
+            print(f'Error removing files: {e}')
+        return response
+
     try:
         return send_file('KPI_files.zip', as_attachment=True)
     except FileNotFoundError:
         abort(404)
-    
 
 if __name__ == '__main__':
-    #app.run(debug=True)
-    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True)
+    #app.run(host='0.0.0.0', port=5000)
 
